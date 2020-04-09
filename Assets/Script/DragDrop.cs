25fd4298d2f2bed18148bc01309a6f81d874f48e
Ryan Beck <rb200@humboldt.edu>
2020-04-09 13:16:33 -0700

    uploading of game

---
+++ Assets/Script/DragDrop.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+
+    private bool isDragging = false;
+    private bool isOverDropZone = false;
+    private GameObject DropZone;
+    private Vector2 startPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+    }
+
+    public void startDrag()
+    {
+        startPosition = transform.position;
+        isDragging = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isOverDropZone = true;
+        DropZone = collision.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isOverDropZone = false;
+        DropZone = null;
+    }
+
+    public void endDrag()
+    {
+        isDragging = false;
+        if (isOverDropZone)
+        {
+            transform.SetParent(DropZone.transform, false);
+        }
+        else
+        {
+            transform.position = startPosition;
+        }
+    }
+}
---
+++ Assets/Script/DrawCards.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject PlayerArea;
+    public GameObject EnemyArea;
+
+    List<GameObject> cards = new List<GameObject>();
+
+    void Start()
+    {
+
+        cards.Add(Card1);
+        cards.Add(Card2);
+        
+
+    }
+
+    public void createCard(GameObject playmat)
+    {
+        GameObject Card = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
+        Card.transform.SetParent(playmat.transform, false);
+    }
+
+    public void onClick()
+    {
+        for (var i = 0; i < 5; i++)
+        {
+            createCard(PlayerArea);
+            createCard(EnemyArea);
+        }
+    }
+
+}
